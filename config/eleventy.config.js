6b761f3709d699422daf3643f393cf3d55e89c5f
Joseph D. Romano <jdromano2@gmail.com>
2020-03-28 14:49:40 -0400

    Add Node and eleventy config

---
+++ config/eleventy.config.js
@@ -0,0 +1,25 @@
+const CleanCSS = require("clean-css");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    })
+
+    eleventyConfig.addPassthroughCopy('src/assets')
+
+    eleventyConfig.addPassthroughCopy('src/assets/icons/favicon.ico')
+
+    return {
+        templateFormats: ["html", "njk", "md", "js"],
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            data: '_data',
+            output: 'www',
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true
+    };
+};
