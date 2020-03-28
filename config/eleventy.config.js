5522c019dd2e9204e0e86b9c6355129d445fc045
Joseph D. Romano <jdromano2@gmail.com>
2020-03-28 16:40:18 -0400

    Make first draft of website

--- config/eleventy.config.js
+++ config/eleventy.config.js
@@ -13,7 +13,7 @@ module.exports = function(eleventyConfig) {
         templateFormats: ["html", "njk", "md", "js"],
         dir: {
             input: 'src',
-            includes: '_includes',
+            includes: 'assets',
             data: '_data',
             output: 'www',
         },
